3ac33d6ada2c86377d306b731dc2efee5b04969e
arturokumura <arturokumura2008@gmail.com>
2026-03-08 19:06:28 -0300

    PUT User

--- src/main/java/com/arturokumura/workshopmongo/services/UserService.java
+++ src/main/java/com/arturokumura/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj) {
+        User newObj = repo.findById(obj.getId()).orElseThrow();
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO objDtO) {
         return new User(objDtO.getId(), objDtO.getName(), objDtO.getEmail());
     }
