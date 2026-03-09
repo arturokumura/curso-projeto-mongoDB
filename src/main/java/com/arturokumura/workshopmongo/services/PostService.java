66bb91fdd51de7a9b3519460dc7b14af76b900be
arturokumura <arturokumura2008@gmail.com>
2026-03-09 17:21:47 -0300

    Consulta simples com @Query

--- src/main/java/com/arturokumura/workshopmongo/services/PostService.java
+++ src/main/java/com/arturokumura/workshopmongo/services/PostService.java
@@ -24,6 +24,6 @@ public class PostService {
     }
 
     public List<Post> findyByTitle(String text) {
-        return repo.findByTitleContainingIgnoreCase(text);
+        return repo.searchTitle(text);
     }
 }
