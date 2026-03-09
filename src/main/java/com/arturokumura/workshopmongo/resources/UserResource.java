2a8be0d6776709009e2def049fb3246a9ce3fe0a
arturokumura <arturokumura2008@gmail.com>
2026-03-09 10:31:18 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/arturokumura/workshopmongo/resources/UserResource.java
+++ src/main/java/com/arturokumura/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.arturokumura.workshopmongo.resources;
 
+import com.arturokumura.workshopmongo.domain.Post;
 import com.arturokumura.workshopmongo.domain.User;
 import com.arturokumura.workshopmongo.dto.UserDTO;
 import com.arturokumura.workshopmongo.services.UserService;
@@ -59,5 +60,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return  ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping (value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity <List<Post>> findPosts(@PathVariable String id) {
+       User obj = service.findById(id);
+        return  ResponseEntity.ok().body(obj.getPosts());
     }
 }
